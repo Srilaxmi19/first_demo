24da17bd2868aec7d26f705dac8155133879cd5f
srila <srila@Angel>
2022-07-20 17:41:44 -0700

    Second Change

--- demo-2/src/main/java/com/example/demo/Demo2Application.java
+++ demo-2/src/main/java/com/example/demo/Demo2Application.java
@@ -8,7 +8,7 @@ public class Demo2Application {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(Demo2Application.class, args);
-		System.out.println("Hello");
+		System.out.println("Hello World from Java Program!");
 	}
 
 }
